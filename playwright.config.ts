c24c6f7c76504a205aecf00ee35a71adae598ed2
Tona <tona@mango-byte.com>
2022-07-10 10:04:49 +0700

    init [unit-testing] components/vue

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { PlaywrightTestConfig, devices } from '@playwright/experimental-ct-vue';
+
+const config: PlaywrightTestConfig = {
+  testDir: 'src',
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        channel: 'chrome',
+        ...devices['Desktop Chrome']
+      },
+    },
+  ],
+};
+
+export default config;
